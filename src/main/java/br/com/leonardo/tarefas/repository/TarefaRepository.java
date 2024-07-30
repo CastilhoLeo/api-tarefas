17faad002f995d18e7e9fcccddbdd3fb6eb9e540
LeonardoC <leonardo_castilho1@hotmail.com>
2024-07-30 13:17:13 -0300

    Criando pesquisa dinamica

--- src/main/java/br/com/leonardo/tarefas/repository/TarefaRepository.java
+++ src/main/java/br/com/leonardo/tarefas/repository/TarefaRepository.java
@@ -1,8 +1,17 @@
 package br.com.leonardo.tarefas.repository;
 
 import br.com.leonardo.tarefas.entity.Tarefa;
+import br.com.leonardo.tarefas.enums.Situacao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
 
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+
+    @Query("select t from Tarefa t where t.dataVencimento between :dataInicio and :dataFim and t.situacao = :situacao")
+    Page<Tarefa> pesquisaDinamica (LocalDate dataInicio, LocalDate dataFim, Situacao situacao, Pageable pageable);
 }
