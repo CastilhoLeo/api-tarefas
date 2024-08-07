66c84064a6a024e103e5c196677d2fa3232b31aa
LeonardoC <leonardo_castilho1@hotmail.com>
2024-08-07 08:01:15 -0300

    finalizando testes do controller

--- src/test/java/br/com/leonardo/tarefas/controller/TarefaControllerTest.java
+++ src/test/java/br/com/leonardo/tarefas/controller/TarefaControllerTest.java
@@ -2,9 +2,9 @@ package br.com.leonardo.tarefas.controller;
 
 import br.com.leonardo.tarefas.builders.TarefaDTOBuilder;
 import br.com.leonardo.tarefas.dto.TarefaDTO;
+import br.com.leonardo.tarefas.enums.Situacao;
 import br.com.leonardo.tarefas.service.TarefaService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.deser.impl.CreatorCandidate;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
@@ -20,7 +20,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -93,7 +92,7 @@ public class TarefaControllerTest {
     }
 
     @Test
-    public void pesquisaDinmica_deveRetornarTodasAsTarefas() throws Exception{
+    public void pesquisaDinmica_devePesquisarComSituacaoNull() throws Exception{
 
         Pageable pageable = PageRequest.of(1,20);
 
@@ -112,14 +111,83 @@ public class TarefaControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/tarefas")
                         .param("situacao", "")
                         .param("page", "1")
-                        .param("size", "20")
+                        .param("size", "20"))
 
-                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.content[0].id", Matchers.nullValue()));
+                .andExpect(jsonPath("$.content[0].id", Matchers.nullValue()))
+                .andExpect(jsonPath("$.content[0].titulo", Matchers.is("teste 1")))
+                .andExpect(jsonPath("$.content[0].descricao", Matchers.is("testeee 1")))
+                .andExpect(jsonPath("$.content[0].dataVencimento", Matchers.is("2024-08-10")))
+                .andExpect(jsonPath("$.content[0].situacao", Matchers.is("PENDENTE")))
+                .andExpect(jsonPath("$.content[1].id", Matchers.nullValue()))
+                .andExpect(jsonPath("$.content[1].titulo", Matchers.is("teste 2")))
+                .andExpect(jsonPath("$.content[1].descricao", Matchers.is("testeee 1")))
+                .andExpect(jsonPath("$.content[1].dataVencimento", Matchers.is("2024-08-10")))
+                .andExpect(jsonPath("$.content[1].situacao", Matchers.is("PENDENTE")));
 
-        Mockito.verify(tarefaService, Mockito.times(1)).pesquisaDinamica(null, pageable);
+        Mockito.verify(tarefaService, Mockito.times(1))
+                .pesquisaDinamica(null, pageable);
+    }
+
+
+    @Test
+    public void pesquisaDinmica_devePesquisarComFiltroDeSituacao() throws Exception{
+
+        Pageable pageable = PageRequest.of(1,20);
+
+        TarefaDTO tarefaDTO1 = TarefaDTOBuilder.criarTarefaDTO();
+        TarefaDTO tarefaDTO2 = TarefaDTOBuilder.criarTarefaDTO();
+        tarefaDTO2.setTitulo("teste 2");
+
+        List<TarefaDTO> lista = new ArrayList<>();
+        lista.add(tarefaDTO1);
+        lista.add(tarefaDTO2);
+
+        Page<TarefaDTO> page = new PageImpl<>(lista);
+
+        Mockito.when(tarefaService.pesquisaDinamica(Situacao.PENDENTE, pageable)).thenReturn(page);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/tarefas")
+                        .param("situacao", "PENDENTE")
+                        .param("page", "1")
+                        .param("size", "20"))
+
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.content[0].id", Matchers.nullValue()))
+                .andExpect(jsonPath("$.content[0].titulo", Matchers.is("teste 1")))
+                .andExpect(jsonPath("$.content[0].descricao", Matchers.is("testeee 1")))
+                .andExpect(jsonPath("$.content[0].dataVencimento", Matchers.is("2024-08-10")))
+                .andExpect(jsonPath("$.content[0].situacao", Matchers.is("PENDENTE")))
+                .andExpect(jsonPath("$.content[1].id", Matchers.nullValue()))
+                .andExpect(jsonPath("$.content[1].titulo", Matchers.is("teste 2")))
+                .andExpect(jsonPath("$.content[1].descricao", Matchers.is("testeee 1")))
+                .andExpect(jsonPath("$.content[1].dataVencimento", Matchers.is("2024-08-10")))
+                .andExpect(jsonPath("$.content[1].situacao", Matchers.is("PENDENTE")));
+
+        Mockito.verify(tarefaService, Mockito.times(1))
+                .pesquisaDinamica(Situacao.PENDENTE, pageable);
+    }
+
+    @Test
+    public void pesquisarPeloId() throws Exception {
+
+        TarefaDTO tarefaDTO = TarefaDTOBuilder.criarTarefaDTO();
+        Mockito.when(tarefaService.localizarPeloId(ArgumentMatchers.anyLong())).thenReturn(tarefaDTO);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/tarefas/{id}",1L ))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", Matchers.nullValue()))
+                .andExpect(jsonPath("$.titulo", Matchers.is("teste 1")))
+                .andExpect(jsonPath("$.descricao", Matchers.is("testeee 1")))
+                .andExpect(jsonPath("$.dataVencimento", Matchers.is("2024-08-10")))
+                .andExpect(jsonPath("$.situacao", Matchers.is("PENDENTE")));
+
+
+        Mockito.verify(tarefaService, Mockito.times(1)).localizarPeloId(1L);
+
     }
 
 
