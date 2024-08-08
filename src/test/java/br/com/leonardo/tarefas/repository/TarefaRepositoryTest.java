b0991b4d6faee1128ea4cc7f88bd9ff8b183f1c3
CastilhoLeo <leonardo_castilho1@hotmail.com>
2024-08-08 19:18:15 -0300

    testes controller

--- src/test/java/br/com/leonardo/tarefas/repository/TarefaRepositoryTest.java
+++ src/test/java/br/com/leonardo/tarefas/repository/TarefaRepositoryTest.java
@@ -3,9 +3,11 @@ package br.com.leonardo.tarefas.repository;
 import br.com.leonardo.tarefas.builders.TarefaBuilder;
 import br.com.leonardo.tarefas.entity.Tarefa;
 import br.com.leonardo.tarefas.enums.Situacao;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,19 +23,15 @@ public class TarefaRepositoryTest {
     @Autowired
     private TarefaRepository tarefaRepository;
 
-    @Autowired
     private Pageable pageable;
 
     @BeforeAll
-    public static void setup(@Autowired TarefaRepository tarefaRepository){
+    public static void setup(@Autowired @NotNull TarefaRepository tarefaRepository){
         Tarefa tarefa1 = TarefaBuilder.criarTarefa();
-        tarefa1.setId(null);
         Tarefa tarefa2 = TarefaBuilder.criarTarefa();
-        tarefa2.setId(null);
         tarefa2.setSituacao(Situacao.FINALIZADO);
         Tarefa tarefa3 = TarefaBuilder.criarTarefa();
-        tarefa3.setId(null);
-        tarefa2.setSituacao(Situacao.EM_ANDAMENTO);
+        tarefa3.setSituacao(Situacao.EM_ANDAMENTO);
 
         List<Tarefa> lista = new ArrayList<>();
         lista.add(tarefa1);
