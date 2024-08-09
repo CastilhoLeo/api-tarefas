c3b258e355cffe74f0ff30bf964e83ddad1eabf6
CastilhoLeo <leonardo_castilho1@hotmail.com>
2024-08-08 22:20:47 -0300

    testes controller

--- src/test/java/br/com/leonardo/tarefas/repository/TarefaRepositoryTest.java
+++ src/test/java/br/com/leonardo/tarefas/repository/TarefaRepositoryTest.java
@@ -3,11 +3,9 @@ package br.com.leonardo.tarefas.repository;
 import br.com.leonardo.tarefas.builders.TarefaBuilder;
 import br.com.leonardo.tarefas.entity.Tarefa;
 import br.com.leonardo.tarefas.enums.Situacao;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,7 +24,7 @@ public class TarefaRepositoryTest {
     private Pageable pageable;
 
     @BeforeAll
-    public static void setup(@Autowired @NotNull TarefaRepository tarefaRepository){
+    public static void setup(@Autowired TarefaRepository tarefaRepository){
         Tarefa tarefa1 = TarefaBuilder.criarTarefa();
         Tarefa tarefa2 = TarefaBuilder.criarTarefa();
         tarefa2.setSituacao(Situacao.FINALIZADO);
