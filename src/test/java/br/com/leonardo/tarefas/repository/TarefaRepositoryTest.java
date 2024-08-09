3ac3d56c6f4b4fcaf20c114c3dd42c7aca0518e8
LeonardoC <leonardo_castilho1@hotmail.com>
2024-08-09 13:03:14 -0300

    finalizando testes

--- src/test/java/br/com/leonardo/tarefas/repository/TarefaRepositoryTest.java
+++ src/test/java/br/com/leonardo/tarefas/repository/TarefaRepositoryTest.java
@@ -3,6 +3,7 @@ package br.com.leonardo.tarefas.repository;
 import br.com.leonardo.tarefas.builders.TarefaBuilder;
 import br.com.leonardo.tarefas.entity.Tarefa;
 import br.com.leonardo.tarefas.enums.Situacao;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,9 +26,12 @@ public class TarefaRepositoryTest {
     @BeforeAll
     public static void setup(@Autowired TarefaRepository tarefaRepository){
         Tarefa tarefa1 = TarefaBuilder.criarTarefa();
+        tarefa1.setId(1L);
         Tarefa tarefa2 = TarefaBuilder.criarTarefa();
+        tarefa2.setId(2L);
         tarefa2.setSituacao(Situacao.FINALIZADO);
         Tarefa tarefa3 = TarefaBuilder.criarTarefa();
+        tarefa3.setId(3L);
         tarefa3.setSituacao(Situacao.EM_ANDAMENTO);
 
         List<Tarefa> lista = new ArrayList<>();
@@ -41,8 +44,10 @@ public class TarefaRepositoryTest {
 
     @Test
     public void findBySituacao(){
-        Page<Tarefa> page = tarefaRepository.findBySituacao(Situacao.PENDENTE, pageable);
+        Page<Tarefa> page = this.tarefaRepository.findBySituacao(Situacao.PENDENTE, pageable);
 
+        Assertions.assertEquals(page.getSize(), 1);
+        Assertions.assertEquals(page.getContent().get(0).getId(), 1L);
 
     }
 
