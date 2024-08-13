c845007c4a1a9fe7cbb1e2fe2db1abe5addc36ef
CastilhoLeo <leonardo_castilho1@hotmail.com>
2024-08-12 22:22:39 -0300

    iniciando front

--- src/test/java/br/com/leonardo/tarefas/TarefasApplicationTests.java
+++ src/test/java/br/com/leonardo/tarefas/TarefasApplicationTests.java
@@ -1,6 +1,8 @@
 package br.com.leonardo.tarefas;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
