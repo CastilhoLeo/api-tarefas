3de691f5575b62b588f0b1c3b952ae8024a86360
CastilhoLeo <leonardo_castilho1@hotmail.com>
2024-08-25 23:04:10 -0300

    finalizando front

---
+++ src/main/resources/static/requests.js
@@ -0,0 +1,78 @@
+function pesquisaTodasTarefas(){
+
+    fetch('http://localhost:8080/tarefas')
+    .then(response=> response.json())
+    .then(data=>{
+    listarTarefas(data.content)
+    }) 
+}
+
+function salvarTarefa(tarefa){
+    fetch(`http://localhost:8080/tarefas`,{
+        method: 'POST',
+        headers :{'Content-Type': 'application/json'},
+        body: JSON.stringify(tarefa)
+    })
+    .then(response=>{
+        if(response.ok){
+            response.json()
+            alert("Tarefa cadastrada com sucesso!")
+            document.getElementById('cadastro').reset();}
+            
+        else{
+             response.json()
+            .then(data => {
+            alert(data.message)
+            console.log(data)
+    })
+}})
+
+    
+}
+
+function deletarTarefa(id){
+    
+    if (confirm(`Confirma a exclusão?`) == true) {
+    
+    fetch(`http://localhost:8080/tarefas/${id}`,{
+        method: 'DELETE'
+    })
+    .then(response => console.log(response))
+
+    location.reload();
+
+}};
+
+function pesquisaSituacao(situacao){
+    fetch(`http://localhost:8080/tarefas?situacao=${situacao}`)
+    .then(response=> response.json())
+    .then(data=>{
+    listarTarefas(data.content)
+    })
+}
+
+
+
+function visualizarTarefa(id){
+    fetch(`http://localhost:8080/tarefas/${id}`)
+    .then(response => response.json())
+    .then (tarefa => {
+        inserirDadosTarefa(tarefa);
+
+        edicao = true;
+        idEdicao = id;
+    
+    });
+    
+}
+
+function editarTarefa(id, tarefa){
+
+    fetch(`http://localhost:8080/tarefas/${id}`, {
+        method: 'PUT',
+        headers :{'Content-Type': 'application/json'},
+        body: JSON.stringify(tarefa)
+    }).then(response => response.json())
+    .then(data => console.log(data))
+       
+}
