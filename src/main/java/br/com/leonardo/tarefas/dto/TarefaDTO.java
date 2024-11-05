98f7a44119073f12fbdf1628613c0f74d9b20398
LeonardoC <leonardo_castilho1@hotmail.com>
2024-11-05 07:41:23 -0300

    ajuste validações

--- src/main/java/br/com/leonardo/tarefas/dto/TarefaDTO.java
+++ src/main/java/br/com/leonardo/tarefas/dto/TarefaDTO.java
@@ -1,6 +1,8 @@
 package br.com.leonardo.tarefas.dto;
 
 import br.com.leonardo.tarefas.enums.Situacao;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -12,10 +14,12 @@ public class TarefaDTO {
 
     private Long id;
 
+    @NotBlank(message = "O preenchimento do título é obrigatório")
     private String titulo;
 
     private String descricao;
 
+    @NotNull(message = "A data de vencimento é obrigatória")
     private LocalDate dataVencimento;
 
     private Situacao situacao;
